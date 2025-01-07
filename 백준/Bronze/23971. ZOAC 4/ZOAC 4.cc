82c52b93f0f14bcda4c9653ae73f2fb3a8ee8729
Wonil Jang <42103354+zeroone-kr@users.noreply.github.com>
2025-01-07 14:24:25 +0900

    [Bronze III] Title: ZOAC 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int H, W, N, M;
+    cin >> H >> W >> N >> M;
+
+    // cout << H << ", " <<  W << ", " <<  N << ", " <<  M << endl;
+    N++; M++;
+    int X = H / N + ((H % N == 0)? 0 : 1);
+    int Y = W / M + ((W % M == 0)? 0 : 1);
+
+    cout << X * Y << endl;
+
+}
