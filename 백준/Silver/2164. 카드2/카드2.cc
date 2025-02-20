bd721b71d0c2dde49273b586c76eb3f1da3334d7
Wonil Jang <42103354+zeroone-kr@users.noreply.github.com>
2025-02-21 08:57:12 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >>n ;
+
+    queue<int> Q;
+
+    for(int i =1 ;i <=n ; ++i){
+        Q.push(i);
+    }
+
+    while(Q.size() > 1){
+        Q.pop();
+        Q.push(Q.front());
+        Q.pop();
+    }
+
+    cout << Q.front() << '\n';
+
+}
