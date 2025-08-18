da59938ac7986b14503a5bb119eae36afe6a4f4c
Wonil Jang <42103354+zeroone-kr@users.noreply.github.com>
2025-08-18 23:27:39 +0900

    [level 2] Title: 전화번호 목록, Time: 90.55 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,7 @@
+def solution(phone_book):
+    phone_book.sort()  
+    for a, b in zip(phone_book, phone_book[1:]):
+        # print(a, b)
+        if b.startswith(a):  
+            return False
+    return True
