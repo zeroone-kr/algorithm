f09c77463511606b932d85fa6b49df83697129ea
Wonil Jang <42103354+zeroone-kr@users.noreply.github.com>
2025-11-09 17:31:22 +0900

    Update 특정 거리의 도시 찾기.py

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,39 +1,35 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
 
 city, roads, diste, start = map(int, input().split())
 
-
 adjM = [[] for _ in range(city + 1)]
-
 
 for _ in range(roads):
     s, d = map(int, input().split())
     adjM[s].append(d)
 
-
-dist = [-1] * (city + 1)
+dist = [0] * (city + 1)
+visited = [False] * (city + 1)
 
 def traverse():
     deq = deque()
     deq.append(start)
-    dist[start] = 0
+    visited[start] = True
 
     while deq:
         src = deq.popleft()
-
         for v in adjM[src]:
-            if dist[v] != -1:
+            if visited[v]:
                 continue
+            visited[v] = True
             dist[v] = dist[src] + 1
             deq.append(v)
 
 traverse()
 
 found = False
-
 for i in range(1, city + 1):
     if dist[i] == diste:
         print(i)
