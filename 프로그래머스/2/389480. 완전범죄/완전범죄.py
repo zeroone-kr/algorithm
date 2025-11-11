5046b643e61dd9323c9628df7fc5185cc7c996d3
Wonil Jang <42103354+zeroone-kr@users.noreply.github.com>
2025-11-11 17:08:34 +0900

    [level 2] Title: 완전범죄, Time: 12.77 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,27 +1,3 @@
 def solution(info, n, m):
-
-    dp = [[False] * m for _ in range(n)]
-    dp[0][0] = True
-
-    for ai, bi in info:
-        nxt = [[False] * m for _ in range(n)]
-        for a in range(n):
-            row = dp[a]
-            for b in range(m):
-                if not row[b]:
-                    continue
-
-                na = a + ai
-                if na < n:
-                    nxt[na][b] = True
-
-                nb = b + bi
-                if nb < m:
-                    nxt[a][nb] = True
-        dp = nxt
-
-
-    for a in range(n):
-        if any(dp[a][b] for b in range(m)):
-            return a
-    return -1
+    answer = 0
+    return answer
